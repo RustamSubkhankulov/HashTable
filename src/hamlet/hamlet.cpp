ecf8ad0ff752a3cb6c26c8979b857d2694cf92ce
RustamSubkhankulov <Rust25102003@gmail.com>
2022-04-10 03:03:30 +0300

    in progress

---
+++ src/hamlet/hamlet.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <assert.h>
+
+#include "hamlet.h"
+#include "../logs/errors_and_logs.h"
+
---
+++ src/hamlet/hamlet.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdio.h>
+
+#include "../logs/errors_and_logs.h"
+#include "hamlet_conf.h"
+
+//===================================================================
+
+
